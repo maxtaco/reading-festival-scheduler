b3c666252efffcaad755d6ce47752f83f14ec41c
Maxwell Krohn <themax@gmail.com>
2012-03-02 21:46:44 -0500

    ok, got this working!

--- scheduler.py
+++ scheduler.py
@@ -10,6 +10,7 @@ class Date :
     def __init__ (self, key, val):
         self._key = int (key)
         self._val = val
+        self._taken = 0
     def key (self):
         return self._key
 
@@ -20,8 +21,8 @@ class Person :
     def __init__ (self, key, val, schedule):
         self._key = key
         self._name = val['name']
-        self._dates = []
-        self._dates = ( schedule._dates[d] for d in val['dates'] )
+        self._date_list = ( schedule._dates[d] for d in val['dates'] )
+        self._date_set = set( [ d._key for d in self._date_list ] )
 
 ##=======================================================================
 
@@ -29,6 +30,7 @@ class Director (Person):
 
     def __init__ (self, key, val, schedule):
         Person.__init__ (self, key, val, schedule)
+        self._num_gigs = 0
 
 ##=======================================================================
 
@@ -36,7 +38,9 @@ class Writer (Person):
 
     def __init__ (self, key, val, schedule):
         Person.__init__ (self, key, val, schedule)
-        self._directors = ( schedule._director[d] for d in val['directors'])
+        l  = ( schedule._director[d] for d in val['directors'])
+        self._director_list = l
+        self._director_set = ( d._key for d in l )
 
 ##=======================================================================
 
@@ -49,14 +53,33 @@ class Schedule:
 
     def load_from_json (self, js):
         for k,v in js['dates'].items():
-            self._dates[k] = Date (k, v)
+            self._dates[int(k)] = Date (k, v)
         for k,v in js['directors'].items():
-            self._directors[k] = Director (k, v, self)
+            self._directors[int(k)] = Director (k, v, self)
         for k,v in js['writers'].items():
-            self._writers[k] = Writer (k, v, self)
+            self._writers[int(k)] = Writer (k, v, self)
 
     def schedule (self) : 
-        pass
+        writers = self._writers.values()
+        res = self._schedule(writers[0], writers[1:])
+        if not res:
+            raise Exception ("no schedules worked!")
+
+    def _schedule(self, w, rest):
+        for date in w._date_list:
+            for k,director in self._directors.values() :
+                if not k in w._director_set and \
+                    director._num_gigs < 2 and \
+                    date._key in director._date_set:
+
+                    director._num_gigs += 1
+                    self._choice = [ date, director ] 
+                    if self._schedule (rest[0], rest[1:]) :
+                        return True
+                    else:
+                        director._num_gigs -= 1
+                        self._choice = None
+        return False
 
     def output (self):
         pass
